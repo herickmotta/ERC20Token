bcb788411f485ef1bae32e781133fbe8f6fb4807
Herick Motta <herickrossato@gmail.com>
2024-01-25 23:13:32 -0300

    feat: create token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+contract ERC20Token is IERC20 {
+    address public owner;
+    string public name;
+    string public symbol;
+
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address account => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event OwnershipTransfer(address indexed previousOwner, address indexed newOwner);
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+        owner = msg.sender;
+        mint(owner, 1 ether);
+    }
+
+    function transfer(address to,uint256 value) external returns (bool success) {
+        return _transferBalance(msg.sender, to, value);
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool success) {
+        _spendAllowance(msg.sender, from, value);
+        return _transferBalance(from, to, value);
+    }
+
+    function _spendAllowance(address caller, address spender, uint256 amount) internal {
+        require(allowance[caller][spender] >= amount, "Not enough allowance");
+        allowance[caller][spender] -= amount;
+    }
+
+    function _transferBalance(address from, address to, uint256 value) internal returns (bool success) {
+        if (from == address(0)) {
+            totalSupply += value;
+        }
+
+        if (to == address(0)) {
+            totalSupply -= value;
+        }
+
+        if (from != address(0) && to != address(0)) {
+            require(balanceOf[from] >= value, "Not enough balance");
+        }
+
+        unchecked {
+            balanceOf[from] -= value;
+            balanceOf[to] += value;
+        }
+
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Invalid new owner address");
+        owner = newOwner;
+        emit OwnershipTransfer(owner, newOwner);
+    }
+
+    function renounceOwnership() public onlyOwner {
+        owner = address(0);
+        emit OwnershipTransfer(owner, address(0));
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _transferBalance(address(0), account, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _transferBalance(account, address(0), amount);
+    }
+}
---
+++ test/Token.spec.js
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("ERC20Token", function () {
+  let ERC20Token;
+  let erc20Token;
+  let owner;
+  let addr1;
+  let addr2;
+
+  beforeEach(async function () {
+    ERC20Token = await ethers.getContractFactory("ERC20Token");
+    [owner, addr1, addr2] = await ethers.getSigners();
+    erc20Token = await ERC20Token.deploy("MyToken", "MTK");
+    await erc20Token.waitForDeployment();
+  });
+
+  it("should have correct initial values", async function () {
+    console.log(owner.address, addr1.address, addr2.address);
+
+    expect(await erc20Token.name()).to.equal("MyToken");
+    expect(await erc20Token.symbol()).to.equal("MTK");
+    expect(await erc20Token.owner()).to.equal(owner.address);
+    expect(await erc20Token.decimals()).to.equal(18);
+    expect(await erc20Token.totalSupply()).to.equal(ethers.parseEther('1'));
+    expect(await erc20Token.balanceOf(owner.address)).to.equal(ethers.parseEther('1'));
+  });
+
+  it("should transfer tokens", async function () {
+    await erc20Token.transfer(addr1.address, 100);
+    expect(await erc20Token.balanceOf(addr1.address)).to.equal(100);
+  });
+
+  it("should approve and transferFrom tokens", async function () {
+    await erc20Token.connect(addr1).approve(owner.address, 100);
+    await erc20Token.connect(addr1).transferFrom(owner.address, addr2.address, 50);
+    expect(await erc20Token.allowance(addr1.address, owner.address)).to.equal(50);
+    expect(await erc20Token.balanceOf(addr2.address)).to.equal(50);
+  });
+
+  it("should transfer ownership", async function () {
+    await erc20Token.transferOwnership(addr1.address);
+    expect(await erc20Token.owner()).to.equal(addr1.address);
+  });
+
+  it("should renounce ownership", async function () {
+    await erc20Token.renounceOwnership();
+    expect(await erc20Token.owner()).to.equal(ethers.ZeroAddress);
+  });
+
+  it("should mint tokens", async function () {
+    const totalSupply = await erc20Token.totalSupply();
+    const ownerBalance = await erc20Token.balanceOf(owner.address);
+
+    await erc20Token.mint(owner.address, 100);
+    expect(await erc20Token.balanceOf(owner.address)).to.equal(ownerBalance + 100n);
+    expect(await erc20Token.totalSupply()).to.equal(totalSupply + 100n);
+  });
+
+  it("should burn tokens", async function () {
+    const totalSupply = await erc20Token.totalSupply();
+    const ownerBalance = await erc20Token.balanceOf(owner.address);
+
+    await erc20Token.burn(owner.address, 50);
+    expect(await erc20Token.balanceOf(owner.address)).to.equal(ownerBalance - 50n);
+    expect(await erc20Token.totalSupply()).to.equal(totalSupply - 50n);
+  });
+});
