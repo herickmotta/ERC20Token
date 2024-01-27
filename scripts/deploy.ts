88fb56aecb9034c633668d5dd463bfdb2b451657
Herick Motta <herickrossato@gmail.com>
2024-01-27 18:26:34 -0300

    fix: deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,7 +4,7 @@ async function main() {
   const ERC20Token = await ethers.getContractFactory("ERC20Token");
   const erc20Token = await ERC20Token.deploy("Heya", "Heya", ethers.parseEther('1'));
   await erc20Token.waitForDeployment();
-  console.log("ERC20Token deployed to:", erc20Token.getAddress());
+  console.log("ERC20Token deployed to:", await erc20Token.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
