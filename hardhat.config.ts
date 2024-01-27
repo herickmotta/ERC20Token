af0679c7acbc393818711a33bf27652d7560f7a8
Herick Motta <herickrossato@gmail.com>
2024-01-27 16:00:31 -0300

    feat: add target network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
+import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
+if (!process.env.NODE_HTTP_URI || !process.env.PRIVATE_KEY) {
+  throw new Error("Missing environment variables");
+}
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.NODE_HTTP_URI,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 
 async function main() {
   const ERC20Token = await ethers.getContractFactory("ERC20Token");
-  const erc20Token = await ERC20Token.deploy("MyToken", "MTK", ethers.parseEther('1'));
+  const erc20Token = await ERC20Token.deploy("Heya", "Heya", ethers.parseEther('1'));
   await erc20Token.waitForDeployment();
   console.log("ERC20Token deployed to:", erc20Token.getAddress());
 }
